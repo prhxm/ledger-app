757bcb79e04740550e60e186e5608b6de3f9149e
prhxm <parhamforfun11@gmail.com>
2025-05-07 03:10:33 -0700

    Update app.py

--- Acc/app.py
+++ Acc/app.py
@@ -131,18 +131,6 @@ def simple_login():
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
-#comment
-   ''' st.markdown(
-        """
-            </div>
-            <div class="honeycomb-img">
-                <img src="https://raw.githubusercontent.com/prhxm/ledger-app/main/assets/honeycomb.png">
-            </div>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )'''
-
     if st.button("Login / Register"):
         if not username or not password:
             st.warning("Please Enter Both Username and Password. 🌱")
